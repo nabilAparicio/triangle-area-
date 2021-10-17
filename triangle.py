e0a0885f2546406265f2f40d005dd6d0f955d702
nabilAparicio <orlando.nabil@gmail.com>
2021-10-17 09:59:11 -0500

    triangle area calculator

---
+++ triangle.py
@@ -0,0 +1,9 @@
+print("welcome to triangle area calculator")
+print("insert the base")
+b = input()
+base = float(b)
+print("now insert the height")
+h = input()
+height = float(h)
+area =(base*height)//2
+print("the area is = ", area) 
